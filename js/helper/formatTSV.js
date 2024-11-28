82c86826fbb305acfcb5a8d5e6673fdc81cb0e05
MarCE-Lab NTU <marcelabntu@gmail.com>
2024-11-28 13:42:43 +0800

    skip empty line

--- js/helper/formatTSV.js
+++ js/helper/formatTSV.js
@@ -1,5 +1,5 @@
 const formatTsv = (data) => {
-  let lines = data.split("\n");
+  let lines = data.split("\n").filter(line => line.trim() !== ""); // Filter out empty lines
   let res = [];
   let headers = lines[0].split("\t");
   console.log("Lines");
@@ -8,8 +8,12 @@ const formatTsv = (data) => {
     var obj = {};
     var currentline = lines[i].split("\t");
 
+    // Skip if the line doesn't have enough columns
+    if (currentline.length !== headers.length) continue;
+  
     for (var j = 0; j < headers.length; j++) {
-      obj[headers[j].replace("\r", "")] = currentline[j].replace("\r", "");
+      //obj[headers[j].replace("\r", "")] = currentline[j].replace("\r", "");
+      obj[headers[j].replace("\r", "")] = currentline[j] ? currentline[j].replace("\r", "") : ""; // add null check
     }
 
     res.push(obj);
